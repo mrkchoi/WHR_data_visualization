2b22098b006ad253dc04493962fc7892f46cedf2
Kenneth Choi <kennethichoi@gmail.com>
2019-07-31 22:03:19 -0700

    Add tooltip to initial gdp graph

--- src/world_graph.js
+++ src/world_graph.js
@@ -30,6 +30,7 @@ class WorldGraph extends Chart {
       // that.setRadius();
       // that.lines();
       that.circles();
+      // that.tooltips();
       // that.percentageLabels();
     });
   }
@@ -39,18 +40,57 @@ class WorldGraph extends Chart {
   // }
 
   circles() {
+      let tooltip = d3.select("body")
+        .append("div")
+        .style("visibility", "hidden")
+        .attr("class", "tooltip")
+        .style("background-color", "black")
+        .style("border-radius", "5px")
+        .style("padding", "10px")
+        .style("color", "white")
+        .style("z-index", "999999999")
+        .style("position", "absolute")
+        .style("display", "block");
 
-    // let radius = d3
-    //   .scaleLinear()
-    //   .domain([10000, 1000000000])
-    //   .range([6, 50]);
-    // debugger;
+      let showTooltip = function(d) {
+        tooltip
+          .transition()
+          .duration(200);
+        tooltip
+          .style("visibility", "visible")
+          .html(
+            `
+              <strong>Country:</strong> ${d.country} (${
+              d.continent
+            })<br/>
+              <strong>Happiness Ranking:</strong> ${d.ranking}
+            `
+          )
+          .style("top", d3.mouse(d3.event.target)[1] + 80 + "px")
+          .style("left", d3.mouse(d3.event.target)[0] - 40 + "px");
+      }
+
+      let moveTooltip = function(d) {
+        showTooltip(d);
+        console.log('move tooltip');
+        tooltip
+          .style("top", d3.mouse(d3.event.target)[1] + 80 + "px")
+          .style("left", d3.mouse(d3.event.target)[0] - 40 + "px")
+      }
+
+      let hideTooltip = function(d) {
+        console.log('hide tooltip');
+        tooltip
+          .transition()
+          .duration(200)
+          .style("visibility", "hidden")
+      }
+
     this.chart
       .selectAll("circle")
       .data(Object.values(this.data))
       .enter()
       .append("circle")
-      // .append("g")
       .attr("class", d => `country ${d.class} country-bubble`)
       .attr("fill", d => {
         if (d.continent === "Africa") {
@@ -70,13 +110,12 @@ class WorldGraph extends Chart {
       .attr("opacity", ".7")
       .attr("stroke", "#CDCDCD")
       .attr("stroke-width", "2px")
-      // .selectAll()
-      // .data(d => d)
-      // .enter()
       .attr("cx", d => {
-        // return 100;
         return this.xScale(d.graphGdp / 156) + 25;
       })
+      .on("mouseover", showTooltip)
+      .on("mousemove", moveTooltip)
+      .on("mouseleave", hideTooltip)
       .transition()
       .delay((d, i) => i * ANIMATION_DELAY)
       .duration(ANIMATION_DURATION)
@@ -91,33 +130,58 @@ class WorldGraph extends Chart {
         } else {
           return d.population / 100000;
         }
-        // debugger
-        // return radius(d.population);
       })
       .attr("cy", d => {
-        // return 100;
         return this.yScale(d.graphRanking / 156);
       });
+      
   }
 
-  // percentageLabels() {
-  //   // debugger;
+  // tooltips() {
+  //   let tooltip = this.chart
+  //     .append("div")
+  //     .style("visibility", "hidden")
+  //     .attr("class", "tooltip")
+  //     .style("background-color", "black")
+  //     .style("border-radius", "5px")
+  //     .style("padding", "10px")
+  //     .style("color", "white");
 
-  //   this.chart
-  //     .selectAll()
-  //     .data(() => Object.values(this.data))
-  //     .enter()
-  //     .append("g")
-  //     .attr("class", d => `city ${d.class} city-data-toggle`)
-  //     .selectAll(".line-point")
-  //     .data(d => d.diversity)
-  //     .enter()
-  //     .append("text")
-  //     .attr("text-anchor", "middle")
-  //     .text(d => d3.format(".0%")(d.percentage))
-  //     .attr("x", d => this.xScale(d.ethnicity) + 25)
-  //     .attr("y", d => this.yScale(d.percentage) - 10)
-  //     .style("fill", "white");
+  //     let showTooltip = d => {
+  //       tooltip
+  //         .transition()
+  //         .duration(200);
+  //       tooltip
+  //         .style("opacity", 1)
+  //         .html(
+  //           `
+  //             Country: ${d.country}<br/>
+  //             Continent: ${d.continent}<br/>
+  //             Happiness Ranking: ${d.ranking}<br/>
+  //             Social Support: ${d.socialSupport}<br/>
+  //             Freedom: ${d.freedom}<br/>
+  //             Generosity: ${d.generosity}<br/>
+  //             GDP Per Capita: ${d.gdp}<br/>
+  //             Life Expectancy: ${d.lifeExpectancy}<br/>
+  //           `
+  //         )
+  //         .style("left", d3.mouse(this)[0] + 30 + "px")
+  //         .style("top", d3.mouse(this)[1] + 30 + "px");
+  //     }
+
+  //     let moveTooltip = d => {
+  //       tooltip
+  //         .style("left", (d3.mouse(this)[0] + 30) + "px")
+  //         .style("top", (d3.mouse(this)[1] + 30) + "px")
+  //     }
+
+  //     let hideTooltip = d => {
+  //       tooltip
+  //         .transition()
+  //         .duration(200)
+  //         .style("opacity", 0)
+  //     }
+
   // }
 }
 
